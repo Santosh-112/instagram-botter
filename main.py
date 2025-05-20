05656c2c89147f1700e8ce5f0f7bdd64b4edcd66
Santosh-112 <lovesantosh688@gmail.com>
2025-05-20 12:32:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os;import version;import github;import owner;import instas;import instagram;import proxies;import colorama; import accounts
-owner.name=("natrix")
+owner.name=("mai_santosh_hoon")
 github.url=("https://github.com/natrixdev") 
 
 requirements = "https://github.com/natrixdev/instagram-botter/blob/main/reqs.txt"
